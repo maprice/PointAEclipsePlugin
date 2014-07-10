46a70a31311437314219ae1bb4f5e6ec1d4e6cfd
psalazar515 <psalazar_92@hotmail.com>
2014-07-09 22:11:57 -0400

    Cleaned up UI Code
    Moved generated files to separate package, and cleaned up some of the UI
    code

--- src/com/pointaeclipseplugin/PointAServiceConstants.java
+++ src/com/pointaeclipseplugin/PointAServiceConstants.java
@@ -13,6 +13,15 @@ public class PointAServiceConstants {
 	// ===========================================================
 	// Constants
 	// ===========================================================
+	
+	//Service Specific Variables
+	public static String[] AdsServiceProviders = { "Admob", "AdSense", "Burstly" };
+	public static String[] AdSizes = { "Banner", "Interstitial" };
+	
+	public static String[] AnalyticsServiceProviders = { "Google Analytics", "Parse Analytics" };
+	public static String[] CrashServiceProviders = { "BugSense", "BugSnag", "Crittercism" };
+	public static String[] PushServiceProviders = { "Parse" };
+	public static String[] RatingServiceProviders = { "Amazon", "Google Play" };
 
 	static final String LOG_TAG =  PointAServiceConstants.class.getSimpleName();
 
--- src/com/pointaeclipseplugin/view/PointAView.java
+++ src/com/pointaeclipseplugin/view/PointAView.java
@@ -14,19 +14,12 @@ import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
+import com.pointaeclipseplugin.PointAServiceConstants;
+
 public class PointAView extends ViewPart {
 	
 	public static final String ID = "PointAEclipsePlugin.view";
 	private TableViewer viewer;
-	
-	//Service Specific Variables
-	String[] AdsServiceProviders = { "Admob", "AdSense", "Burstly" };
-	String[] AdSizes = { "Banner", "Interstitial" };
-	
-	String[] AnalyticsServiceProviders = { "Google Analytics", "Parse Analytics" };
-	String[] CrashServiceProviders = { "BugSense", "BugSnag", "Crittercism" };
-	String[] PushServiceProviders = { "Parse" };
-	String[] RatingServiceProviders = { "Amazon", "Google Play" };
 	
 	class ViewContentProvider implements IStructuredContentProvider{
 		public void inputChanged(Viewer v, Object oldInput, Object newInput){}
@@ -41,117 +34,92 @@ public class PointAView extends ViewPart {
 		}
 	}
 	
+	// ===========================================================
+	// Helper Methods
+	// ===========================================================
+	//User Input Generator
 	private void makeTextInput(Composite c, String label){
 		new Label(c, SWT.LEFT).setText(label);
 		new Text(c, SWT.BORDER);
 	}
-	
 	private void makeDropdown(Composite c, String label, String [] options){
 		new Label(c,SWT.LEFT).setText(label);
 		new Combo(c, SWT.DROP_DOWN).setItems(options);
 	}
-
-	private Control AdsTabControl(TabFolder tabFolder){
+	
+	//GridLayout Composite Generator
+	private Composite getGridComposite(TabFolder tabFolder){
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 2;
 		Composite composite = new Composite(tabFolder, SWT.NONE);
 		composite.setLayout(gridLayout);
-		makeDropdown(composite, "Service Provider", AdsServiceProviders);
-		makeDropdown(composite, "Ad Size", AdSizes);
+		return composite;
+	}
+	
+	//Tab configuration
+	private TabItem createTab(TabFolder tabFolder, String text, Control c){
+		TabItem tab = new TabItem(tabFolder, SWT.NONE);
+		tab.setText(text);
+		tab.setControl(c);
+		return tab;
+	}
+	
+	// ===========================================================
+	// Tab Control Configuration (FIX: MAKE CONFIGURATIONS DYNAMIC) 
+	// ===========================================================
+	private Control AdsTabControl(TabFolder tabFolder){
+		Composite composite = getGridComposite(tabFolder);
+		makeDropdown(composite, "Service Provider", PointAServiceConstants.AdsServiceProviders);
+		makeDropdown(composite, "Ad Size", PointAServiceConstants.AdSizes);
 		makeTextInput(composite, "Unit ID");
 		return composite; 
 	}
-	
 	private Control AnalyticsTabControl(TabFolder tabFolder){
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		Composite composite = new Composite(tabFolder, SWT.NONE);
-		composite.setLayout(gridLayout);
-		makeDropdown(composite, "Service Provider", AdsServiceProviders);
+		Composite composite = getGridComposite(tabFolder);
+		makeDropdown(composite, "Service Provider", PointAServiceConstants.AdsServiceProviders);
 		makeTextInput(composite, "App ID");
 		makeTextInput(composite, "Client Key");
 		return composite;   
 	}
-	
 	private Control CrashTabControl(TabFolder tabFolder){
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		Composite composite = new Composite(tabFolder, SWT.NONE);
-		composite.setLayout(gridLayout);
-		makeDropdown(composite, "Service Provider", CrashServiceProviders);
+		Composite composite = getGridComposite(tabFolder);
+		makeDropdown(composite, "Service Provider", PointAServiceConstants.CrashServiceProviders);
 		makeTextInput(composite, "App ID");
 		return composite;   
 	}
-	
 	private Control RatingTabControl(TabFolder tabFolder){
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		Composite composite = new Composite(tabFolder, SWT.NONE);
-		composite.setLayout(gridLayout);
-		makeDropdown(composite, "Service Provider", RatingServiceProviders);
+		Composite composite = getGridComposite(tabFolder);
+		makeDropdown(composite, "Service Provider", PointAServiceConstants.RatingServiceProviders);
 		makeTextInput(composite, "URI");
 		return composite;   
 	}
-	
 	private Control PushNotificationsTabControl(TabFolder tabFolder){
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		Composite composite = new Composite(tabFolder, SWT.NONE);
-		composite.setLayout(gridLayout);
-		makeDropdown(composite, "Service Provider", PushServiceProviders);
+		Composite composite = getGridComposite(tabFolder);
+		makeDropdown(composite, "Service Provider", PointAServiceConstants.PushServiceProviders);
 		makeTextInput(composite, "App ID");
 		makeTextInput(composite, "Client Key");
 		return composite;   
 	}
-	
 	private Control TwitterTabControl(TabFolder tabFolder){
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		Composite composite = new Composite(tabFolder, SWT.NONE);
-		composite.setLayout(gridLayout);
+		Composite composite = getGridComposite(tabFolder);
 		makeTextInput(composite, "Consumer Key");
 		makeTextInput(composite, "Consumer Secret");
 		return composite;   
 	}
 	
+	// ===========================================================
+	// Populate Tab Folder
+	// ===========================================================
 	public void createPartControl(Composite parent) {
 		final TabFolder tabFolder = new TabFolder(parent, SWT.NONE);
 		
-		//Ads Tab
-		TabItem Tab_Ads = new TabItem(tabFolder, SWT.NONE);
-        Tab_Ads.setText("Ads");
-        Tab_Ads.setToolTipText("Ads Configurations");  
-        Tab_Ads.setControl(AdsTabControl(tabFolder));
-        
-        //Analytics Tab
-        TabItem Tab_Analytics = new TabItem(tabFolder, SWT.NONE);
-        Tab_Analytics.setText("Analytics");
-        Tab_Analytics.setToolTipText("Analytics Configurations");
-        Tab_Analytics.setControl(AnalyticsTabControl(tabFolder));
-        
-        //Crash Reporting Tab
-        TabItem Tab_Crash = new TabItem(tabFolder, SWT.NONE);
-        Tab_Crash.setText("Crash Reporting");
-        Tab_Crash.setToolTipText("Crash Reporting Configurations");
-        Tab_Crash.setControl(CrashTabControl(tabFolder));
-        
-        //Rating Tab
-        TabItem Tab_Rating = new TabItem(tabFolder, SWT.NONE);
-        Tab_Rating.setText("Rating");
-        Tab_Rating.setToolTipText("Rating Configurations");
-        Tab_Rating.setControl(RatingTabControl(tabFolder));
-        
-        //Push Notifications Tab
-        TabItem Tab_PushNotifications = new TabItem(tabFolder, SWT.NONE);
-        Tab_PushNotifications.setText("Push Notifications");
-        Tab_PushNotifications.setToolTipText("Push Notifications Configurations");
-        Tab_PushNotifications.setControl(PushNotificationsTabControl(tabFolder));
-        
-        //Twitter Tab
-        TabItem Tab_Twitter = new TabItem(tabFolder, SWT.NONE);
-        Tab_Twitter.setText("Twitter");
-        Tab_Twitter.setToolTipText("Twitter Configurations");
-        Tab_Twitter.setControl(TwitterTabControl(tabFolder));
+		//Create Tabs
+		TabItem Tab_Ads = createTab(tabFolder, "Ads", AdsTabControl(tabFolder));
+        TabItem Tab_Analytics = createTab(tabFolder, "Analytics", AnalyticsTabControl(tabFolder));
+        TabItem Tab_Crash = createTab(tabFolder, "Crash Reporting", CrashTabControl(tabFolder));
+        TabItem Tab_Rating = createTab(tabFolder, "Rating", RatingTabControl(tabFolder));
+        TabItem Tab_PushNotifications = createTab(tabFolder, "Push Notifications", PushNotificationsTabControl(tabFolder));
+        TabItem Tab_Twitter = createTab(tabFolder, "Twitter", TwitterTabControl(tabFolder));
 	}
 
 	public void setFocus() {
