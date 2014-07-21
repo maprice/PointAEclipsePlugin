e7241eb4cda0c9954d39d3dcd970359d327af802
Mike Price <maprice35@gmail.com>
2014-07-21 17:28:52 -0400

    Create sample provider object

---
+++ src/com/pointaeclipseplugin/model/MasterProviderDoc.java
@@ -0,0 +1,36 @@
+package com.pointaeclipseplugin.model;
+
+import java.util.ArrayList;
+
+public class MasterProviderDoc {
+	
+	
+	// Could have a list of all providers,  OR a hashmap with key = serviceType value = ArrayList<Providers> (Better) 
+	final ArrayList<Provider> providerMasterList;
+	
+	
+	public MasterProviderDoc(){
+		providerMasterList = new ArrayList<Provider>();
+		
+		//Populate providerMasterList
+		// Will be populated via JSON from server in future, but for now just hardcode
+		
+		
+		// Create admob
+		Provider admob = new Provider();
+		// Populate admob data
+		admob.name = admob;
+		
+		
+		// Add admob to list
+		providerMasterList.add(admob);
+		
+		//Create other stuff..........
+		
+	}
+	
+	// UI reads me
+	public ArrayList<Provider> getProviders(){
+		return providerMasterList;
+	}
+}
---
+++ src/com/pointaeclipseplugin/model/Provider.java
@@ -0,0 +1,14 @@
+package com.pointaeclipseplugin.model;
+
+import java.util.HashMap;
+
+public class Provider {
+	String name;
+	int type; // make enum
+	String jarLocation;
+	
+	HashMap<String,String> params;
+	
+	
+	
+}
