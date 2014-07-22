2c8cb14d1496ba0ff7701193096e608a28794959
psalazar515 <psalazar_92@hotmail.com>
2014-07-21 20:26:43 -0400

    Updated MasterProvider + Provider

--- src/com/pointaeclipseplugin/model/MasterProviderDoc.java
+++ src/com/pointaeclipseplugin/model/MasterProviderDoc.java
@@ -1,9 +1,9 @@
 package com.pointaeclipseplugin.model;
 
 import java.util.ArrayList;
+import com.pointaeclipseplugin.model.PointAServiceConstants.Services;
 
 public class MasterProviderDoc {
-	
 	
 	// Could have a list of all providers,  OR a hashmap with key = serviceType value = ArrayList<Providers> (Better) 
 	final ArrayList<Provider> providerMasterList;
@@ -12,18 +12,83 @@ public class MasterProviderDoc {
 	public MasterProviderDoc(){
 		providerMasterList = new ArrayList<Provider>();
 		
-		//Populate providerMasterList
-		// Will be populated via JSON from server in future, but for now just hardcode
+		//====================
+		// ADS
+		//====================
+		Provider admob = new Provider("Admob", Services.Ads, null);
+		admob.params.add("Size");
+		admob.params.add("Unit ID");
 		
+		Provider adsense = new Provider("AdSense", Services.Ads, null);
+		admob.params.add("Size");
+		admob.params.add("Unit ID");
 		
-		// Create admob
-		Provider admob = new Provider();
-		// Populate admob data
-		admob.name = "admob";
+		Provider burstly = new Provider("Burstly", Services.Ads, null);
+		admob.params.add("Size");
+		admob.params.add("Unit ID");
 		
-		
-		// Add admob to list
 		providerMasterList.add(admob);
+		providerMasterList.add(adsense);
+		providerMasterList.add(burstly);
+		
+		//====================
+		// ANALYTICS
+		//====================
+		Provider googleanalytics = new Provider("Google Analytics", Services.Analytics, null);
+		googleanalytics.params.add("App ID");
+		googleanalytics.params.add("Client Key");
+		
+		Provider parseanalytics = new Provider("Parse Analytics", Services.Analytics, null);
+		googleanalytics.params.add("App ID");
+		googleanalytics.params.add("Client Key");
+		
+		providerMasterList.add(googleanalytics);
+		providerMasterList.add(parseanalytics);
+		
+		//====================
+		// CRASH
+		//====================
+		Provider bugsense = new Provider("BugSense", Services.CrashReporter, null);
+		bugsense.params.add("App ID");
+		
+		Provider bugsnag = new Provider("BugSnag", Services.CrashReporter, null);
+		bugsnag.params.add("App ID");
+		
+		Provider crittercism = new Provider("Crittercism", Services.CrashReporter, null);
+		crittercism.params.add("App ID");
+		
+		providerMasterList.add(bugsense);
+		providerMasterList.add(bugsnag);
+		providerMasterList.add(crittercism);
+		
+		//====================
+		// PUSH
+		//====================
+		Provider parse = new Provider("Parse", Services.Push, null);
+		parse.params.add("App ID");
+		parse.params.add("Client Key");
+		providerMasterList.add(parse);
+		
+		//====================
+		// RATING
+		//====================
+		Provider amazon = new Provider("Amazon", Services.Rating, null);
+		amazon.params.add("URI");
+		
+		Provider googleplay = new Provider("Google Play", Services.Rating, null);
+		googleplay.params.add("URI");
+		
+		providerMasterList.add(amazon);
+		providerMasterList.add(googleplay);
+		
+		//====================
+		// TWITTER
+		//====================
+		Provider twitter = new Provider("Twitter", Services.Twitter, null);
+		twitter.params.add("Consumer Key");
+		twitter.params.add("Consumer Secret");
+		
+		providerMasterList.add(twitter);
 		
 		//Create other stuff..........
 		
--- src/com/pointaeclipseplugin/model/Provider.java
+++ src/com/pointaeclipseplugin/model/Provider.java
@@ -1,14 +1,21 @@
 package com.pointaeclipseplugin.model;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pointaeclipseplugin.model.PointAServiceConstants.Services;
 
 public class Provider {
-	String name;
-	int type; // make enum
-	String jarLocation;
+	public String name;
+	public Services type; // make enum
+	public String jarLocation;
 	
-	HashMap<String,String> params;
+	List<String> params;
 	
-	
-	
+	public Provider(String name, Services type, String jarLocation){
+		this.name = name;
+		this.type = type;
+		this.jarLocation = jarLocation;
+		this.params = new ArrayList<String>();
+	}	
 }
